fcf487db63d043df700f7ba0b5a9a4ffb771de20
Hossam Aldokkani <hossam.aldokkani@vodafone.com>
2021-01-18 01:09:11 +0200

    WIP

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
 console.log("index.js");
+Notification.requestPermission(function (status) {
+  console.log("Notification permission status:", status);
+  // displayNotification();
+});
+
+function displayNotification() {
+  if (Notification.permission == "granted") {
+    navigator.serviceWorker.getRegistration().then(function (reg) {
+      reg.showNotification("Hello world!");
+    });
+  }
+}
+
+// if ("serviceWorker" in navigator) {
+//   navigator.serviceWorker
+//     .register("sw.js")
+//     .then(function (reg) {
+//       console.log("Service Worker Registered!", reg);
+
+//       reg.pushManager.getSubscription().then(function (sub) {
+//         if (sub === null) {
+//           // Update UI to ask user to register for Push
+//           console.log("Not subscribed to push service!");
+//         } else {
+//           // We have a subscription, update the database
+//           console.log("Subscription object: ", sub);
+//         }
+//       });
+//     })
+//     .catch(function (err) {
+//       console.log("Service Worker registration failed: ", err);
+//     });
+// }
